d8b68fb24ae39cc116b359d6bab447741a6dcaf4
Raphael Traviss <raphael@skyleafdesign.com>
2018-02-04 21:24:12 -0600

    Initial Commit

---
+++ famliar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  famliar
+//
+//  Created by Raphael on 2/4/18.
+//  Copyright © 2018 Skyleaf Design LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
